2847250216ef9ea646c13f41ff82361ca3a26d8a
Dawid <wit.dawid@interia.pl>
2020-06-29 17:20:08 +0200

    First commit

---
+++ src/main/java/com/kodilla/walletmenager/WalletmenagerApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.walletmenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletmenagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletmenagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/walletmenager/WalletmenagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.walletmenager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletmenagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
