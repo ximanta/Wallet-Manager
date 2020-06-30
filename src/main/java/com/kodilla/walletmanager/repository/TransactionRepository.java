c423bcab9ab87be43320f53a562045a85b4a14d3
Dawid <wit.dawid@interia.pl>
2020-06-30 17:02:50 +0200

    WMP001-009 Utworzyć tabele Transaction

---
+++ src/main/java/com/kodilla/walletmanager/repository/TransactionRepository.java
@@ -0,0 +1,6 @@
+package com.kodilla.walletmanager.repository;
+
+import com.kodilla.walletmanager.domain.Transaction;
+
+public interface TransactionRepository extends GenericEntityRepository<Transaction>{
+}
