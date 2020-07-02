6b54804e20ece4870403c19aa2dc047781507fe2
Dawid <wit.dawid@interia.pl>
2020-07-02 22:34:20 +0200

    WMP001-023 TransactionTest dodanie testu createUncompletedRecordTest

--- src/test/java/com/kodilla/walletmanager/domain/TransactionTest.java
+++ src/test/java/com/kodilla/walletmanager/domain/TransactionTest.java
@@ -15,8 +15,7 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.Calendar;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 @Transactional
 @RunWith(SpringRunner.class)
@@ -49,6 +48,33 @@ public class TransactionTest {
         assertEquals(localDate.getDayOfMonth(),fromDb.getDate().toLocalDate().getDayOfMonth());
         assertEquals(localDate.getMonthValue(),fromDb.getDate().toLocalDate().getMonthValue());
         assertEquals(localDate.getYear(),fromDb.getDate().toLocalDate().getYear());
+
+        transactionRepository.delete(fromDb);
+        assertFalse(transactionRepository.existsById(transaction.getId()));
+    }
+
+    @Test
+    public void createUncompletedRecordTest(){
+        //Given
+        Transaction transaction = new Transaction();
+        transaction.setTitle("Test");
+        transaction.setType(TransactionType.REVENUES);
+        transaction.setDate(Date.valueOf("2018-06-25"));
+
+
+        transactionRepository.save(transaction);
+
+        //When
+        Transaction fromDb = transactionRepository.getOne(transaction.getId());
+
+        //Then
+        assertEquals("Test",fromDb.getTitle());
+        assertNull(transaction.getDescription());
+        assertEquals(0,fromDb.getAmount(),0);
+        assertEquals(TransactionType.REVENUES,fromDb.getType());
+        assertEquals(25,fromDb.getDate().toLocalDate().getDayOfMonth());
+        assertEquals(6,fromDb.getDate().toLocalDate().getMonthValue());
+        assertEquals(2018,fromDb.getDate().toLocalDate().getYear());
 
         transactionRepository.delete(fromDb);
         assertFalse(transactionRepository.existsById(transaction.getId()));
