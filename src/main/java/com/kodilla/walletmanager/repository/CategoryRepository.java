6a35076b05ac026057c031fb6af5d638341b5633
Dawid <wit.dawid@interia.pl>
2020-07-04 21:30:14 +0200

    WMP001-025 Utworzyć encję Category

---
+++ src/main/java/com/kodilla/walletmanager/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.kodilla.walletmanager.repository;
+
+import com.kodilla.walletmanager.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
