46758b3b0d29950d1cfa944eb7b374194ce6d225
Dawid <wit.dawid@interia.pl>
2020-07-11 14:34:11 +0200

    WMP001-045 Dodać Tranaction time endopint.

--- src/main/java/com/kodilla/walletmanager/controller/TransactionController.java
+++ src/main/java/com/kodilla/walletmanager/controller/TransactionController.java
@@ -1,6 +1,5 @@
 package com.kodilla.walletmanager.controller;
 
-import com.kodilla.walletmanager.domain.Transaction;
 import com.kodilla.walletmanager.dto.TransactionDto;
 import com.kodilla.walletmanager.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,7 @@ public class TransactionController {
     @Autowired
     TransactionService transactionService;
 
+    //CRUD
     @PostMapping("/create")
     public TransactionDto create(@RequestBody TransactionDto dto){
         return transactionService.create(dto);
@@ -37,5 +37,31 @@ public class TransactionController {
     @DeleteMapping("/delete/{id}")
     public boolean delete(@PathVariable long id){
         return transactionService.delete(id);
+    }
+
+    //DATE
+    @GetMapping("/byDate/{date}")
+    public List<TransactionDto> findByDate(@PathVariable String date){
+        return transactionService.findByDate(date);
+    }
+
+    @GetMapping("/thisWeek")
+    public List<TransactionDto> thisWeek(){
+        return transactionService.thisWeek();
+    }
+
+    @GetMapping("/thisMonth")
+    public List<TransactionDto> thisMonth(){
+        return transactionService.thisMonth();
+    }
+
+    @GetMapping("/selectedMonth/{year_month}")
+    public List<TransactionDto> selectedMonth(@PathVariable String year_month){
+        return transactionService.selectedMonth(year_month);
+    }
+
+    @GetMapping("/betweenDate/{fromDate}/{toDate}")
+    public List<TransactionDto> betweenDate(@PathVariable String fromDate,@PathVariable String toDate) {
+        return transactionService.betweenDate(fromDate, toDate);
     }
 }
