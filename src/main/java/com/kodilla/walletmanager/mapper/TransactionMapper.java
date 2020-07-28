1fdf8073b93f1cbcdce90a84dd31948b1ec79004
Ragnar <wit.dawid@interia.pl>
2020-07-28 20:54:05 +0200

    WMP001-060 Update TransactionMapper and UserMapper

--- src/main/java/com/kodilla/walletmanager/mapper/TransactionMapper.java
+++ src/main/java/com/kodilla/walletmanager/mapper/TransactionMapper.java
@@ -1,10 +1,13 @@
 package com.kodilla.walletmanager.mapper;
 
-import com.kodilla.walletmanager.domain.entities.Category;
-import com.kodilla.walletmanager.domain.entities.Transaction;
+import com.kodilla.walletmanager.domain.builders.TransactionBuilder;
+import com.kodilla.walletmanager.domain.builders.TransactionDtoBuilder;
 import com.kodilla.walletmanager.domain.dto.CategoryDto;
 import com.kodilla.walletmanager.domain.dto.TransactionDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.kodilla.walletmanager.domain.dto.UserDto;
+import com.kodilla.walletmanager.domain.entities.Category;
+import com.kodilla.walletmanager.domain.entities.Transaction;
+import com.kodilla.walletmanager.domain.entities.User;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,31 +15,40 @@ import java.util.stream.Collectors;
 
 @Component
 public class TransactionMapper {
-    @Autowired
     CategoryMapper categoryMapper;
+    UserMapper userMapper;
+
+    private TransactionMapper(CategoryMapper categoryMapper, UserMapper userMapper) {
+        this.categoryMapper = categoryMapper;
+        this.userMapper = userMapper;
+    }
 
     public Transaction mapToEntity(TransactionDto transactionDto){
         Category category = categoryMapper.mapToEntity(transactionDto.getCategoryDto());
-        return new Transaction.TransactionBuilder()
+        User user = userMapper.mapToEntity(transactionDto.getUserDto());
+        return new TransactionBuilder()
                 .id(transactionDto.getId())
                 .title(transactionDto.getTitle())
                 .description(transactionDto.getDescription())
                 .amount(transactionDto.getAmount())
                 .type(transactionDto.getType())
                 .date(transactionDto.getDate())
-                .category(category).build();
+                .category(category)
+                .user(user).build();
     }
 
     public TransactionDto mapToDto(Transaction transaction){
         CategoryDto categoryDto = categoryMapper.mapToDto(transaction.getCategory());
-        return new TransactionDto.TransactionDtoBuilder()
+        UserDto userDto = userMapper.mapToDto(transaction.getUser());
+        return new TransactionDtoBuilder()
                 .id(transaction.getId())
                 .title(transaction.getTitle())
                 .description(transaction.getDescription())
                 .amount(transaction.getAmount())
                 .type(transaction.getType())
                 .date(transaction.getDate())
-                .category(categoryDto).build();
+                .category(categoryDto)
+                .user(userDto).build();
     }
 
     public List<TransactionDto> mapToDtos(List<Transaction> transactions){
--- src/main/java/com/kodilla/walletmanager/mapper/UserMapper.java
+++ src/main/java/com/kodilla/walletmanager/mapper/UserMapper.java
@@ -1,30 +1,35 @@
 package com.kodilla.walletmanager.mapper;
 
+import com.kodilla.walletmanager.domain.builders.UserBuilder;
+import com.kodilla.walletmanager.domain.builders.UserDtoBuilder;
 import com.kodilla.walletmanager.domain.entities.User;
 import com.kodilla.walletmanager.domain.dto.UserDto;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class UserMapper {
     public User mapToEntity(UserDto dto){
-        User user = new User();
-        user.setId(dto.getId());
-        user.setEmile(dto.getEmile());
-        user.setBalance(dto.getBalance());
-
-        return user;
+        return new UserBuilder()
+                .login(dto.getLogin())
+                .password(dto.getPassword())
+                .emile(dto.getEmile())
+                .birthDate(dto.getBirthDate())
+                .active(dto.isActive())
+                .balance(dto.getBalance()).build();
     }
 
     public UserDto mapToDto(User user){
-        UserDto dto = new UserDto();
-        dto.setId(user.getId());
-        dto.setEmile(user.getEmile());
-        dto.setBalance(user.getBalance());
-
-        return dto;
+        return new UserDtoBuilder()
+                .login(user.getLogin())
+                .password(user.getPassword())
+                .emile(user.getEmile())
+                .birthDate(user.getBirthDate())
+                .active(user.isActive())
+                .balance(user.getBalance()).build();
     }
 
     public List<UserDto> mapToDtos(List<User> users){
